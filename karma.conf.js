866d2956b8030068d58754904cc4717adb3a328d
Joey <61996287+Joeybervin@users.noreply.github.com>
2021-12-15 08:43:28 +0100

    ajout supp

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Musics'),
+      dir: require('path').join(__dirname, './coverage/SingingCar'),
       subdir: '.',
       reporters: [
         { type: 'html' },
